7f3929c2d03bd349d992102c4470dda8b5fd93b1
Nikhil <>
2019-01-09 04:46:49 -0500

    Revert "Compiled and Tested"
    This reverts commit 37c0015e31fd0b10a59ddcc8d9ed6544819e9b31.

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,60 +13,49 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) internal balances;
+    mapping (address => uint) balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) public enrolled;
+    mapping (address => bool) enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address public owner;
+    address owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled(address indexed accountAddress);
+    event LogEnrolled();
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade(address indexed accountAddress, uint indexed amount);
+    event LogDepositMade();
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address indexed accountAddress, uint indexed withdrawAmount, uint indexed newBalance);
+
 
     //
     // Functions
     //
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() public {
+    constructor() {
         /* Set the owner to the creator of this contract */
-        owner = msg.sender;
-
     }
 
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function balance() public view returns (uint) {
+    function balance() public returns (uint) {
         /* Get the balance of the sender of this transaction */
-
-        return balances[msg.sender];
-    
-
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool _Status){
-        enrolled[msg.sender] = true;
-        balances[msg.sender] = 0;
-        _Status = true;
-        emit LogEnrolled(msg.sender);
-
+    function enroll() public returns (bool){
     }
 
     /// @notice Deposit ether into bank
@@ -74,12 +63,9 @@ contract SimpleBank {
     // Add the appropriate keyword so that this function can receive ether
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
-    function deposit() public payable returns (uint) {
+    function deposit() public returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
-        balances[msg.sender] += msg.value;
-        emit LogDepositMade(msg.sender, msg.value);
-        return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -92,13 +78,6 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-        require (balances[msg.sender] >= withdrawAmount,"balance should be greater than or equal to withdrawal");
-        balances[msg.sender] -= withdrawAmount;
-        msg.sender.transfer(withdrawAmount);
-        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
-        return balances[msg.sender];
-
-
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -106,7 +85,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() external {
-        revert("invalid data transaction will be reverted");
+    function() {
+        revert();
     }
 }
